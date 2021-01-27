3f07d1ef4aa33df4b8e46b20719e9020b93472db
Olzhas <a.olzhas.a@gmail.com>
2021-01-27 11:53:53 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-token = '1564539502:AAGXaVj5PwNdCw-GhSs2DBfXO_nZ0nF5jDE' # bot token from @BotFather
+token = '' # bot token from @BotFather
 
-WeatherApi = '77f28b3faf4fb0360d4bbc87dc8569c0' # weather api from https://openweathermap.org/
+WeatherApi = '' # weather api from https://openweathermap.org/
 
 weatherURL = 'http://api.openweathermap.org/data/2.5/weather'
 
